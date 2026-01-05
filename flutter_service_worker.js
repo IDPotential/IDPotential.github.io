bfdd6c7c62a97289778b3a501b7ede99acc7effd
IDPotential <IDPotential@users.noreply.github.com>
2026-01-05 14:01:19 +0000

    deploy: 912e7ecbc170be5e96047a3baeba32d34302e1e0

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -9,7 +9,7 @@ const RESOURCES = {"flutter.js": "24bc71911b75b5f8135c949e27a2984e",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "manifest.json": "a667cf404403961ebaea48905ccce3ad",
-"zoom_service.js": "bd9013eea1a2a9b403fb1a1c723d26a3",
+"zoom_service.js": "766bb80597966540b6d86b51dfc40b97",
 "index.html": "6bc906ea53c1d2c0adc50a198d7d28db",
 "/": "6bc906ea53c1d2c0adc50a198d7d28db",
 "assets/shaders/stretch_effect.frag": "40d68efbbf360632f614c731219e95f0",
@@ -81,10 +81,10 @@ const RESOURCES = {"flutter.js": "24bc71911b75b5f8135c949e27a2984e",
 "canvaskit/canvaskit.js.symbols": "a3c9f77715b642d0437d9c275caba91e",
 "canvaskit/skwasm.js.symbols": "3a4aadf4e8141f284bd524976b1d6bdc",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"flutter_bootstrap.js": "2b621874e5cffc11eb2f77f013c48860",
+"flutter_bootstrap.js": "8baefd9227f9632abe331817adeca6d1",
 "coi-serviceworker.js": "f4e6e0859e3bb0a3c96a7604fc2f2df1",
 "version.json": "4b41496a10ce4e9c664a0e375a85baf2",
-"main.dart.js": "38e58f8a52ba05386a7dde52539ee488"};
+"main.dart.js": "fb447fd71608d98786f02d953c4de44b"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
