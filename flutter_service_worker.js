73d074da282659bc4db229c3a4ae0b980b4300d0
IDPotential <IDPotential@users.noreply.github.com>
2026-01-08 00:23:25 +0000

    deploy: a5e6b4d49e1319f128cb342b157af9d1341c6fb2

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -84,7 +84,7 @@ const RESOURCES = {"flutter.js": "24bc71911b75b5f8135c949e27a2984e",
 "canvaskit/canvaskit.js.symbols": "a3c9f77715b642d0437d9c275caba91e",
 "canvaskit/skwasm.js.symbols": "3a4aadf4e8141f284bd524976b1d6bdc",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"flutter_bootstrap.js": "b89c58aecaf27f7bb5778036d199527e",
+"flutter_bootstrap.js": "dfd597be8a589e350f68586917b53695",
 "coi-serviceworker.js": "f4e6e0859e3bb0a3c96a7604fc2f2df1",
 "version.json": "3c4bd82b92ef038478f16ce9b0434e98",
 "main.dart.js": "2035adc8c5a4eeabede9e9ba1b411548"};
