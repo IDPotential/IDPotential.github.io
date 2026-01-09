3df9bf38c722ed86ba635f5f972711e0df0c6755
IDPotential <IDPotential@users.noreply.github.com>
2026-01-09 10:35:37 +0000

    deploy: a8b4251ab719db0217cdd94b4a284a9a74a0bc0e

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -84,10 +84,10 @@ const RESOURCES = {"flutter.js": "24bc71911b75b5f8135c949e27a2984e",
 "canvaskit/canvaskit.js.symbols": "a3c9f77715b642d0437d9c275caba91e",
 "canvaskit/skwasm.js.symbols": "3a4aadf4e8141f284bd524976b1d6bdc",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"flutter_bootstrap.js": "36eb4795534c5ecaba701a5220374c13",
+"flutter_bootstrap.js": "e922ca24373fbd304c9e9116ce26b073",
 "coi-serviceworker.js": "f4e6e0859e3bb0a3c96a7604fc2f2df1",
 "version.json": "c9f71cfeda9beafebe878455e0dd3a8a",
-"main.dart.js": "cb0edf8e3fa6e2ebf95796cbf9bce274"};
+"main.dart.js": "fe90cf19edce4c2f7de5a735f1e4b0fd"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
