e4ff1ebe16c6371bb6581000eb957165143a2845
IDPotential <IDPotential@users.noreply.github.com>
2026-01-15 22:41:23 +0000

    deploy: 3377430c33ee4e0497162441c858b5da0e509da2

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"78fc3012e45889657f72359b005af7beac47ba
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1355521045"
+    serviceWorkerVersion: "3422836980"
   }
 });
