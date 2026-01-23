f70eed4198af158d9d52e61c830423917ec12d65
IDPotential <IDPotential@users.noreply.github.com>
2026-01-23 15:54:56 +0000

    deploy: d79ab16e6b2c8d53e9b9c6c2f57464bd27b6a48b

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"78fc3012e45889657f72359b005af7beac47ba
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "801755126"
+    serviceWorkerVersion: "1408483031"
   }
 });
--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -89,7 +89,7 @@ const RESOURCES = {"flutter.js": "24bc71911b75b5f8135c949e27a2984e",
 "canvaskit/skwasm.js.symbols": "3a4aadf4e8141f284bd524976b1d6bdc",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "404.html": "aceaba07b123eb8b4d6029540feb9907",
-"flutter_bootstrap.js": "651257745693ca1fdf511c8c5f8a1d62",
+"flutter_bootstrap.js": "e10c5b5f24c0912c68df6edceda4957c",
 "coi-serviceworker.js": "f4e6e0859e3bb0a3c96a7604fc2f2df1",
 "version.json": "0763dfe58856e85779b5fbcf55f5602f",
 "main.dart.js": "2593a7afb4b90b7d97eafa67723c6c34"};
