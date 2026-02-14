ff5e0bdf580783057f4e02ea95697370e83f7c27
IDPotential <IDPotential@users.noreply.github.com>
2026-02-14 01:16:05 +0000

    deploy: 1740a769a777350733a4f35cdf196c1cfe2e369f

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"3452d735bd38224ef2db85ca763d862d6326b1
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2087110018" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "100091006" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
